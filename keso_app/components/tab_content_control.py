a0c5d2c1b929fd4fce2560cc2204d50627175ed2
Julio Cordero <julioccorderoc@gmail.com>
2025-01-04 14:55:07 -0400

    tab component created
    + Lacks tailwind styles to make it look like pills

---
+++ keso_app/components/tab_content_control.py
@@ -0,0 +1,14 @@
+import reflex as rx
+from typing import List
+
+def tab_content_control(
+    tab_values: List[str],
+    tab_state: rx.State
+    ) -> rx.Component:
+    triggers = [rx.tabs.trigger(value, value=value) for value in tab_values]
+    return rx.tabs.root(
+        rx.tabs.list(*triggers),
+        default_value=tab_values[0] if tab_values else None,
+        value=tab_state.selected_tab,
+        on_change=tab_state.set_selected_tab,
+    )   
