e27993dd2e1af915c69573865f13ec74da5df309
Julio Cordero <julioccorderoc@gmail.com>
2025-01-04 14:55:43 -0400

    tabs state created

--- keso_app/pages/production.py
+++ keso_app/pages/production.py
@@ -1,11 +1,24 @@
 import reflex as rx
 from ..states import routes
-from .. import styles
 from ..template.base import create_page
+from .. import styles
+from ..components.tab_content_control import tab_content_control
+
+PRODUCTION_TABS = ["Leche", "Queso", "Suero"]
+class Production_Table_State(rx.State):
+    selected_tab: str = PRODUCTION_TABS[0]
+
+    @rx.event
+    def set_selected_tab(self, value: str):
+        self.selected_tab = value
 
 @create_page(route=routes.PRODUCTION_ROUTE, title="Producción")
 def production():
+
     return rx.vstack(
         rx.heading("Mi Contenido"),
         rx.text("Algo de contenido..."),
+        tab_content_control(PRODUCTION_TABS, Production_Table_State),
+        rx.text(f"{Production_Table_State.selected_tab}  clicked !"),
+        
     )
