bb3619420f8531ff6facf12b27d0a23e6f4ec26e
Julio Cordero <julioccorderoc@gmail.com>
2025-01-13 21:51:50 -0400

    testing the dialog-form-addDB

--- keso_app/pages/transactions.py
+++ keso_app/pages/transactions.py
@@ -1,9 +1,28 @@
 import reflex as rx
 from ..states import routes
 from .. import styles
+
 from ..template.base import create_page
 from ..components import dialog_form
-from ..crud_pages import forms
+from ..crud_pages import add_milk_batch_form, Milk_Batches_DB, models
+
+trigger_button_config = {
+    "button_text": "Nueva Transacción",
+    "icon": "plus"
+}
+
+header_config = {
+    "icon": "milk",
+    "title": "Nueva Transacción",
+    "description": "Este es una dialogo de prueba", 
+}
+
+action_buttons_config = {
+    "on_close": rx.dialog.close(), 
+    "on_submit": Milk_Batches_DB.handle_submit(),
+    "submit_button_text": "Registrar leche",
+    "cancel_button_text": "Cancel",
+}
 
 
 
@@ -12,4 +31,10 @@ def transactions():
     return rx.vstack(
         rx.heading("Mi Contenido"),
         rx.text("Algo de contenido..."),
+        dialog_form(
+            trigger_button_config,
+            header_config,
+            add_milk_batch_form,
+            Milk_Batches_DB.handle_submit(),
+        )
     )
