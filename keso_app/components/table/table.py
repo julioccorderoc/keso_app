d1001d39d59cba1f74b2f056e0a1ea971cdc756c
julioccorderoc <julioccorderoc@gmail.com>
2025-04-27 15:03:48 -0400

    WORKING

--- keso_app/components/table/table.py
+++ keso_app/components/table/table.py
@@ -5,6 +5,7 @@ from keso_app.components.table.pagination import pagination
 # from keso_app.states.cheese_state import ModelType, CheeseBase
 
 # TODO: type hints should match the state to be used, not be a generic state
+# TODO: tackle the default value for pagination, currently it's 10 even if internally it's different
 
 table_style = {
     
@@ -45,27 +46,37 @@ def display_headers(headers: list[str]) -> rx.Component:
     ),
 
 # Rows
-def _create_cell(cell_value: Any) -> rx.Component:
-    return rx.table.cell(cell_value)  
+def _create_cell(cell_data: tuple[str, Any]) -> rx.Component:
+    _, value = cell_data
+    return rx.table.cell(value)
 
-def _create_row(row_data: dict) -> rx.Component:
-    # fields = list(row_data.__dict__.values())
+def _get_visible_cell(column: str, row_data: dict) -> tuple[str, Any]:
+    return (column, row_data[column])
+
+def _create_row(data_state, row_data: dict) -> rx.Component:
     return rx.table.row(
         rx.foreach(
-            row_data.values(),
-            _create_cell
-        ),
-        # style = row_on_hover_style,
-        # on_click = .select_db_entry(row_data)
+            data_state.visible_columns,
+            lambda column: _create_cell(
+                _get_visible_cell(
+                    column, 
+                    row_data
+                )
+            )
+        )
     )
 
-def display_body(rows: list[dict]) -> rx.Component:
+def display_body(data_state) -> rx.Component:
     return rx.table.body(
         rx.foreach(
-            rows,
-            _create_row
-        ),
+            data_state.current_page_data,
+            lambda row: _create_row(
+                data_state, 
+                row
+            )
+        )
     )
+
 
 # # Skeleton for loading state
 # def skeleton_cell(_: rx.Var) -> rx.Component:
@@ -94,10 +105,10 @@ def table_component(data_state) -> rx.Component:
     return rx.vstack(
         rx.table.root(
             display_headers(data_state.visible_columns),
-            display_body(data_state.current_page_data), # add loading logic
-            variant="surface",
-            on_mount=data_state.fetch_data,
-            width="100%",
+            display_body(data_state), # add loading logic
+            variant = "surface",
+            on_mount = data_state.fetch_data,
+            width = "100%",
         ),
         pagination(data_state), # TODO: add a skeleton loader matching the size of pagination
         width="100%",
