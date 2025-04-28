304f2a02e35752f7bf9b71ce76149984a9392bd5
julioccorderoc <julioccorderoc@gmail.com>
2025-04-27 22:48:10 -0400

    add new implementation

--- keso_app/pages/cattle.py
+++ keso_app/pages/cattle.py
@@ -2,13 +2,23 @@ import reflex as rx
 from ..template.base import create_page
 from ..states import cheese_state, routes
 from ..components.table.table import table_component
+from ..components.table.pagination import pagination
+from ..components.table.controls import table_controls
 from .. import styles
+
+def data_display(data_state) -> rx.Component:
+    return rx.vstack(
+        table_controls(data_state),
+        table_component(data_state),
+        pagination(data_state),
+        width="100%",
+        padding="4",
+    )
+
 
 @create_page(route=routes.CATTLE_ROUTE, title="Ganado")
 def cattle():
     return rx.vstack(
-        rx.heading("Mi Contenido"),
-        rx.text("Algo de contenido..."),
-        table_component(cheese_state.CheeseState),
+        data_display(cheese_state.CheeseState),
         width="100%",
     )
