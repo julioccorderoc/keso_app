7951b2d193e6d501e0c784c014cc060d5ed45e95
julioccorderoc <julioccorderoc@gmail.com>
2025-04-27 22:46:36 -0400

    add table labels

--- keso_app/components/table/table.py
+++ keso_app/components/table/table.py
@@ -1,11 +1,7 @@
 import reflex as rx
 from typing import Any
 
-from keso_app.components.table.pagination import pagination
-# from keso_app.states.cheese_state import ModelType, CheeseBase
-
 # TODO: type hints should match the state to be used, not be a generic state
-# TODO: tackle the default value for pagination, currently it's 10 even if internally it's different
 
 table_style = {
     
@@ -25,17 +21,18 @@ row_on_hover_style = {
 }
 
 # Headers
-def _create_header(header: str) -> rx.Component:
+def _create_header(header: tuple[str, str]) -> rx.Component:
+    label = header[1]
     return rx.table.column_header_cell(
         rx.hstack(
             # rx.icon(icon, size=18),
-            rx.text(header),
+            rx.text(label),
             align="center",
             spacing="2",
         )
     )
 
-def display_headers(headers: list[str]) -> rx.Component:
+def display_headers(headers: list[tuple[str, str]]) -> rx.Component: # TODO: center headers
     return rx.table.header(
         rx.table.row(
             rx.foreach(
@@ -43,27 +40,44 @@ def display_headers(headers: list[str]) -> rx.Component:
                 _create_header
             ),
         ),
-    ),
+    )
 
 # Rows
-def _create_cell(cell_data: tuple[str, Any]) -> rx.Component:
-    _, value = cell_data
-    return rx.table.cell(value)
+def _create_cell(data_state, value: Any) -> rx.Component:
+    return rx.table.cell(
+        rx.skeleton(
+            value,
+            loading=data_state.is_loading,
+            height="20px",
+            width="100%",
+        )
+    )
 
-def _get_visible_cell(column: str, row_data: dict) -> tuple[str, Any]:
-    return (column, row_data[column])
+def _get_visible_cell(column: tuple[str, str], row_data: dict) -> Any:
+    value = column[0]
+    return row_data[value]
 
 def _create_row(data_state, row_data: dict) -> rx.Component:
     return rx.table.row(
         rx.foreach(
             data_state.visible_columns,
             lambda column: _create_cell(
+                data_state,
                 _get_visible_cell(
                     column, 
                     row_data
                 )
             )
-        )
+        ),
+        style = {
+            "_hover": {
+                "background_color": "#f3f4f6",  # Tailwind's gray-100
+                "transition": "background-color 150ms",
+            },
+            "cursor": "pointer",
+            "transition": "background-color 150ms",
+        },
+        on_click = data_state.select_db_entry(row_data)
     )
 
 def display_body(data_state) -> rx.Component:
@@ -77,40 +91,11 @@ def display_body(data_state) -> rx.Component:
         )
     )
 
-
-# # Skeleton for loading state
-# def skeleton_cell(_: rx.Var) -> rx.Component:
-#     return rx.table.cell(
-#         rx.skeleton(
-#             rx.spinner()
-#         )
-#     )
-
-# def skeleton_row(data_state) -> rx.Component:
-#     return rx.table.row(
-#         rx.foreach(
-#             data_state.visible_columns,
-#             lambda col: skeleton_cell(col)
-#         )
-#     )
-
-# def display_skeleton_body(data_state) -> rx.Component:
-#     return rx.foreach(
-#         data_state.skeleton_range,
-#         skeleton_row(data_state)
-#     )
-
-
 def table_component(data_state) -> rx.Component:
-    return rx.vstack(
-        rx.table.root(
-            display_headers(data_state.visible_columns),
-            display_body(data_state), # add loading logic
-            variant = "surface",
-            on_mount = data_state.fetch_data,
-            width = "100%",
-        ),
-        pagination(data_state), # TODO: add a skeleton loader matching the size of pagination
-        width="100%",
-        padding="4",
+    return rx.table.root(
+        display_headers(data_state.visible_columns),
+        display_body(data_state), # add loading logic
+        variant = "surface",
+        on_mount = data_state.fetch_data,
+        width = "100%",
     )
