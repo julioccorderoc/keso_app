756e5a742548390b66b684ebe1891c0820c3e7bf
julioccorderoc <julioccorderoc@gmail.com>
2025-04-29 08:47:44 -0400

    add meny dropdown for filters

--- keso_app/components/table/controls.py
+++ keso_app/components/table/controls.py
@@ -5,6 +5,8 @@ from keso_app.constants.shared import NO_FILTER_LABEL, NO_FILTER_VALUE
 def _action_button(data_state) -> rx.Component:
     return rx.button(
         rx.text("Actions"),
+        radius = "large",
+        variant = "solid",
         # on_click = data_state.toggle_actions
     )
 
@@ -16,14 +18,21 @@ def _sort_control(data_state) -> rx.Component:
                 rx.icon("arrow-down-a-z"),
                 rx.icon("arrow-down-z-a"),
             ),
+            radius = "large",
+            variant = "ghost",
             on_click=data_state.toggle_sort_direction,
         ),
         rx.select.root(
-            rx.select.trigger(radius = "full", variant = "soft"),
+            rx.select.trigger(radius = "large", variant = "surface"),
             rx.select.content(
-                # VISIBLE_COLUMNS, TODO: get from the constants
+                rx.select.group(
+                    rx.foreach(
+                        data_state.sortable_columns,
+                        lambda column: rx.select.item(column[1], value = column[0])
+                    )
+                ),
             ),
-            value = "default",
+            value = data_state.column_sorted,
             on_change = data_state.handle_sort_column,
         ),
         spacing = "2",
@@ -35,7 +44,7 @@ def _column_filter(data_state, column_to_filter) -> rx.Component:
     column_name = column_to_filter[0]
     filter_options = column_to_filter[1]
     return rx.select.root(
-        rx.select.trigger(placeholder = column_name, radius = "full", variant = "soft"),
+        rx.select.trigger(placeholder = column_name, radius = "large", variant = "surface"),
         rx.select.content(
             rx.foreach(
                 filter_options,
@@ -48,7 +57,7 @@ def _column_filter(data_state, column_to_filter) -> rx.Component:
 
 def _columns_filter(data_state) -> rx.Component:
     return rx.menu.root(
-        rx.menu.trigger(rx.text("Filters"), radius = "full", variant = "soft"),
+        rx.menu.trigger(rx.text("Filters"), radius = "large", variant = "surface"),
         rx.menu.content(
             rx.foreach(
                 data_state.filters_for_ui,
@@ -65,25 +74,40 @@ def _search_filter(data_state) -> rx.Component:
             rx.icon(tag="search"),
         ),
         placeholder="Search here...",
+        radius = "large",
+        variant = "surface",
         on_change = data_state.handle_search_query.debounce(500),
     )
 
 def _export_button(data_state) -> rx.Component:
     return rx.button(
         rx.icon("download"),
-        radius = "full",
+        radius = "large",
+        variant = "surface",
         on_click = data_state.download_csv,
         disabled = False, # TODO: disable if no data is available or loading is true
     )
 
-# TODO: replace category filter with a filter dropdown component
+# TODO: render controls conditionally, depending on the data
 def table_controls(data_state) -> rx.Component:
     return rx.flex(
-        _action_button(data_state), # conditionally, depending on the data
-        _sort_control(data_state), # conditionally, depending on the data
-        _columns_filter(data_state), # conditionally, depending on the data
         rx.hstack(
-            _search_filter(data_state), # conditionally, depending on the data
-            _export_button(data_state),
+            _action_button(data_state),
+            spacing = "4",
         ),
+        rx.hstack(
+            _sort_control(data_state),
+            _columns_filter(data_state),
+            rx.hstack(
+                _search_filter(data_state),
+                _export_button(data_state),
+                spacing = "5",
+            ),
+            spacing = "5",
+        ),
+        justify="between",
+        align="center",
+        width="100%",
+        flex_direction=["column", "row", "row"],
+        spacing="5",
     )
